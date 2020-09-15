889b84eb065df812d90ea711f86a74a7c4993be4
OmerAbdulSamad110 <41125159+OmerAbdulSamad110@users.noreply.github.com>
2020-09-15 19:54:26 +0500

    added routes for thread reply notification

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,35 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('/threads', 'ThreadController');
-Route::get('/threads/{slug}/{thread}', 'ThreadController@show');
-Route::post('/threads/{slug}/{thread}/reply', 'ReplyController@store');
+
+Route::get('/threads', 'ThreadController@index');
+Route::get('/threads/create', 'ThreadController@create');
+Route::get('/threads/{channel}/{thread}', 'ThreadController@show');
+Route::delete('/threads/{channel}/{thread}', 'ThreadController@destroy');
+Route::post('/threads', 'ThreadController@store');
+Route::get('/threads/{channel}', 'ThreadController@index');
+
+Route::post('/threads/{channel}/{thread}/subscriptions', 'ThreadSubscriptionController@store')
+    ->middleware('auth');
+Route::delete('/threads/{channel}/{thread}/subscriptions', 'ThreadSubscriptionController@destroy')
+    ->middleware('auth');
+
+Route::post('/replies/{reply}/favorites', 'FavoriteController@store');
+Route::delete('/replies/{reply}/favorites', 'FavoriteController@destroy');
+
+Route::get('/threads/{channel}/{thread}/replies', 'ReplyController@index');
+Route::post('/threads/{channel}/{thread}/replies', 'ReplyController@store');
+Route::put('/replies/{reply}', 'ReplyController@update');
+Route::delete('/replies/{reply}', 'ReplyController@destroy');
+
+Route::get('/profile/{user}', 'ProfileController@show');
+
+Route::get('/notifications', 'UserNotificationController@index');
+Route::get('/notifications/{notifications}/read', 'UserNotificationController@markAsRead');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::post('api/users/{user}/avatar', 'Api\UserAvatarController@store')
+    ->middleware('auth');
